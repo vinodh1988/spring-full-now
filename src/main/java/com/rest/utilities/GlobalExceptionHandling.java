3875d5104ba39d346dd83c0fe377b863cf266212
vinodh1988 <k.c.vinodh@hotmail.com>
2025-07-03 12:30:30 +0530

    latest

--- src/main/java/com/rest/utilities/GlobalExceptionHandling.java
+++ src/main/java/com/rest/utilities/GlobalExceptionHandling.java
@@ -1,5 +1,7 @@
 package com.rest.utilities;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandling {
+	Logger logger = LoggerFactory.getLogger(GlobalExceptionHandling.class);
 	@ExceptionHandler(value= {IllegalArgumentException.class})
 	protected ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e){
 		 String response= e.getMessage();
@@ -16,6 +19,7 @@ public class GlobalExceptionHandling {
 	@ExceptionHandler(value= {RecordNotFoundException.class})
 	protected ResponseEntity<Object> handleRecordNotFoundException(RecordNotFoundException e){
 		 String response= e.getMessage();
+		 logger.error("Record not found: {}", response);
 		 return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
 	}
 	
